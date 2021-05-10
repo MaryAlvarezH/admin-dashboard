fa66b1e99c45dfd790e45ecde38909175e1550ba
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-05-10 11:42:42 -0500

    add basic template to sentiment-analysis component

--- src/app/modules/dashboard/pages/sentiment-analysis/sentiment-analysis.component.ts
+++ src/app/modules/dashboard/pages/sentiment-analysis/sentiment-analysis.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SentimentAnalysisComponent implements OnInit {
 
+  activeTabView = 1;
+
   constructor() { }
 
   ngOnInit(): void {
